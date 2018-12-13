c3df4a13c988f20e99cca2f85428f4768f0f2f95
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2018-12-13 20:18:31 -0300

    modifica formulario de registro

---
+++ js/register.js
@@ -0,0 +1,16 @@
+var formulario = document.getElementById("register")
+
+formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var datos = new FormData(formulario);
+
+    fetch('http://localhost:8000/vendedor/', {
+        method: 'POST',
+        body: datos
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+})
