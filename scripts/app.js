4f72cce0acd54b6e8a6daa4c0dfd77b0864bd054
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2018-12-13 19:00:52 -0300

    Arregla problemas con los productos en la página

--- scripts/app.js
+++ scripts/app.js
@@ -1,73 +1,81 @@
-(function() {
+(function () {
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-                .register('./service-worker.js')
-                .then(function() {
-                    console.log('Service Worker Registered');
-        });
+            .register('./service-worker.js')
+            .then(function () {
+                console.log('Service Worker Registered');
+            });
     }
-})( );
+})();
 
-(  function() {
+(function () {
     var app = {
         /* bearColorFilter: document.getElementById( "bearColorFilter" ), */
         productoList: [],
     }
 
-    var loadData = function() {
+    var loadData = function () {
         var xhttp = new XMLHttpRequest();
         var url = "http://127.0.0.1:8000/productos/";
 
-        xhttp.onreadystatechange = function() {
-            if( this.readyState == 4 && this.status == 200 ){
-                console.log( this.responseText );
-                var data = JSON.parse( this.responseText );
-                displayProductos( data.results );
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+                var data = JSON.parse(this.responseText);
+                displayProductos(data.results);
                 app.productoList = data.results;
             }
         }
-        xhttp.open( 'GET', url, true );
+        xhttp.open('GET', url, true);
         xhttp.send();
     }
 
-    var displayProductos = function( productos ) {
-        var productosContainer = document.getElementById( "productosContainer");
+    var displayProductos = function (productos) {
+        var productosContainer = document.getElementById("productosContainer");
         productosContainer.innerHTML = "";
 
-        for( let producto of productos ) {
-            var productoContainer = document.createElement( "div" );
-            var txtproductoName = document.createElement( "h3" );
-            var imgProducto = document.createElement( "img" );
-            var txtProductoDescripcion = document.createElement( "p" );
-            var txtProductoPrecio = document.createElement( "p" );
-            var txtProductoTipo = document.createElement( "p" );
-            productoContainer.className = "productoContainer";
-            txtproductoName.innerHTML = producto.nombre; //Le tenia name, y la variable era nombre... ademas la api anterior traia problemas
+        for (let producto of productos) {
+            var productoContainer = document.createElement("div");
+            var card = document.createElement("div");
+            var cardBody = document.createElement("div");
+            var txtproductoName = document.createElement("h3");
+            var imgProducto = document.createElement("img");
+            var txtProductoDescripcion = document.createElement("p");
+            var txtProductoPrecio = document.createElement("p");
+            var txtProductoTipo = document.createElement("p");
+            productoContainer.className = "col-lg-4 col-md-6 mb-4";
+            card.className = "card h-100";
+            cardBody.className = "card-body";
+            imgProducto.className = "card-img-top";
             imgProducto.src = producto.imageUrl;
             imgProducto.alt = producto.nombre;
             txtProductoDescripcion.innerHTML = producto.descripcion;
-            txtProductoPrecio.innerHTML = producto.precio;
+            txtProductoPrecio.innerHTML = "<b>$: </b>" + producto.precio;
             txtProductoTipo.innerHTML = "<b>Tipo Producto: </b>" + producto.tipo;
-            // Agregar los hijos correspondientes
-            productoContainer.appendChild( txtproductoName );
-            productoContainer.appendChild( imgProducto );
-            productoContainer.appendChild( txtProductoDescripcion );
-            productoContainer.appendChild( txtProductoPrecio );
-            productoContainer.appendChild( txtProductoTipo );
+            txtproductoName.className = "card-title";
+            txtproductoName.innerHTML = '<a href="#">' + producto.nombre + '</a>';
+            // Agregar los hijos correspondientes\
+            productoContainer.appendChild(card);
+            card.appendChild(imgProducto);
+            card.appendChild(cardBody);
+            cardBody.appendChild(txtproductoName);
+            cardBody.appendChild(txtProductoDescripcion);
+            cardBody.appendChild(txtProductoPrecio);
+            cardBody.appendChild(txtProductoTipo);
             // Agregar el contenedor al documento
-            productosContainer.appendChild( productoContainer );
+            productosContainer.appendChild(productoContainer);
         }
     }
 
-/*     app.bearColorFilter.addEventListener( "change", function( e ) {
-        var filteredBears = app.bearList.filter( function( bear ) {
-            if( bear.color == app.bearColorFilter.value ) {
-                return bear;
-            }
-        } );
-        displayProductos( filteredBears );
-    } ); */
-    
+    /*     app.bearColorFilter.addEventListener( "change", function( e ) {
+            var filteredBears = app.bearList.filter( function( bear ) {
+                if( bear.color == app.bearColorFilter.value ) {
+                    return bear;
+                }
+            } );
+            displayProductos( filteredBears );
+        } ); */
+
     loadData();
-} ) ( );
+})();
